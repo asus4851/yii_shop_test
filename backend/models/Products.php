b3147657baaa4954f27d04341747b6204b909d39
asus4851 <hres.vadym@gmail.com>
2015-10-31 15:18:06 +0200

    create a table products

---
+++ backend/models/Products.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $description
+ * @property integer $number
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'description', 'number'], 'required'],
+            [['number'], 'integer'],
+            [['name'], 'string', 'max' => 25],
+            [['description'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'number' => 'Number',
+        ];
+    }
+
+    
+}
